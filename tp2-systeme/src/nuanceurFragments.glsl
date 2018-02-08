49716f9e6968509ce12c58d5a12b71b46beeffde
HHqZz <constantinbouis@gmail.com>
2018-02-08 13:52:49 -0500

    nuanceur fragment fonctionnel

--- tp2-systeme/src/nuanceurFragments.glsl
+++ tp2-systeme/src/nuanceurFragments.glsl
@@ -20,18 +20,14 @@ void main( void )
    {
       // Obtenir la distance à la caméra du sommet dans le repère de la caméra
       float dist = gl_FragCoord.z / gl_FragCoord.w ;
+
       // Obtenir un facteur d'interpolation entre 0 et 1
-      float factDist = smoothstep( debAttenuation,finAttenuation,dist );
+      float factDist = smoothstep( finAttenuation,debAttenuation,dist );
+
       // Modifier la couleur du fragment en utilisant ce facteur
-       
+      FragColor = factDist * FragColor ;
 
 
-      // pour déboguer et « voir » la dist, on peut utiliser:
-     // FragColor = vec4( vec3(dist-floor(dist)), 1.0 );
-      //FragColor = vec4( vec3(factDist), 1.0 );
+     
    }
-
-   // pour déboguer et « voir » le comportement de z ou w, on peut utiliser:
-   //FragColor = vec4( vec3(gl_FragCoord.z), 1.0 );
-   //FragColor = vec4( vec3(gl_FragCoord.w), 1.0 );
 }
